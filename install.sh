e94246555109cbdc55a4576f02002ac89ede272d
Robert Peteuil <robert@peteuil.com>
2017-03-25 10:07:22 -0700

    Revised Installer - Replace Makefile and Function for Local Install and Install from Online Repo

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 REPOACCT="robertpeteuil"
 REPONAME="aws-quick-cli"
+REPORTPHRASE="${REPONAME} utilities"
 
 # exit if AWS CLI utilities not present
 if ! type "aws" &> /dev/null; then
@@ -9,20 +10,69 @@ if ! type "aws" &> /dev/null; then
   exit
 fi
 
-# switch to temp dir
-TMPDIR=${TMPDIR:-/tmp}
-cd "${TMPDIR}" || exit 1
+CURRENTDIR="$(pwd)"
+CURRENTDIR=${CURRENTDIR##*\/}
+UTILTMPDIR="${REPOACCT}-${REPONAME}"
 
-# find latest repo package, download and un-tar it
-PKGDLURL=$(curl -s https://api.github.com/repos/${REPOACCT}/${REPONAME}/releases/latest | grep tarball_url | head -n 1 | cut -d '"' -f 4)
-curl -sL "$PKGDLURL" | tar zxf -
+cleanupDownload() {
+  cd "${TMPDIR}" || exit 1
+  rm -rf "${UTILTMPDIR}"
+}
 
-# find the name of the dir extracted to, as it has the last commit # as a suffix
-UTILTMPDIR=$(ls -d -t ${REPOACCT}-${REPONAME}* | head -n 1)
-UTILTMPDIR=${UTILTMPDIR//\//}
+# download the latest release from github UNLESS
+#   current dir-name = repo name (from git clone) OR
+#   current dir-name = package DL name (from extracting tar or zip)
+if [[ ! ("${CURRENTDIR}" =~ "${UTILTMPDIR}"*) && ! (("${CURRENTDIR}" =~ "${REPONAME}"*)) ]]; then
 
-# switch to extraction dir, install, then remove extraction dir & files
-cd "${UTILTMPDIR}" || exit 1
-make install
-cd "${TMPDIR}" || exit 1
-rm -rf "${UTILTMPDIR}"
+  # find latest repo package, download and un-tar it
+  TMPDIR=${TMPDIR:-/tmp}
+  cd "${TMPDIR}" || exit 1
+  PKGDLURL=$(curl -s https://api.github.com/repos/${REPOACCT}/${REPONAME}/releases/latest | grep tarball_url | head -n 1 | cut -d '"' -f 4)
+  curl -sL "$PKGDLURL" | tar zxf -
+
+  # find the name of the dir extracted to, as it has the last commit # as a suffix
+  UTILTMPDIR=$(ls -d -t ${REPOACCT}-${REPONAME}* | head -n 1)
+  UTILTMPDIR=${UTILTMPDIR/%\//}
+  cd "${UTILTMPDIR}" || exit 1
+  CLEANUPREQ=true
+fi
+
+# determine destination
+if [[ -w "/usr/local/bin" ]]; then
+  BINDIR="/usr/local/bin"
+  CMDPREFIX=""
+  STREAMLINED=true
+else
+  echo -e "installer - ${REPORTPHRASE}\n"
+  echo "Specify install directory (a,b or c):"
+  echo -en "\t(a) '~/bin'    (b) '/usr/local/bin' as root    (c) abort : "
+  read -r -n 1 SELECTION
+  echo
+  if [ "${SELECTION}" == "a" ] || [ "${SELECTION}" == "A" ]; then
+    BINDIR="${HOME}/bin"
+    CMDPREFIX=""
+  elif [ "${SELECTION}" == "b" ] || [ "${SELECTION}" == "B" ]; then
+    BINDIR="/usr/local/bin"
+    CMDPREFIX="sudo "
+  else
+    [[ "${CLEANUPREQ}" ]] && cleanupDownload
+    exit 0
+  fi
+fi
+
+# install
+mkdir -p "${BINDIR}"
+${CMDPREFIX} cp -f aws-info "${BINDIR}"
+${CMDPREFIX} cp -f aws-list "${BINDIR}"
+${CMDPREFIX} cp -f aws-ssh "${BINDIR}"
+${CMDPREFIX} cp -f aws-start "${BINDIR}"
+${CMDPREFIX} cp -f aws-stop "${BINDIR}"
+
+if [[ "${CLEANUPREQ}" ]]; then
+  cleanupDownload
+fi
+
+[[ ! "$STREAMLINED" ]] && echo
+echo "${REPORTPHRASE} installed to ${BINDIR}"
+
+exit 0
