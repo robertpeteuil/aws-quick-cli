677f4d77dedd54f645f141b74e50b77072ccc085
Robert Peteuil <robert@peteuil.com>
2017-04-27 11:40:26 -0700

    Test - ignore unused var warnings as this file is sourced

--- utils/aws-quick-cli-init.sh
+++ utils/aws-quick-cli-init.sh
@@ -24,18 +24,18 @@ if test -t 1; then            # check if stdout is a terminal
     green="$(tput setaf 2)"
     yellow="$(tput setaf 3)"
     blue="$(tput setaf 4)"
-    magenta="$(tput setaf 5)"
+    magenta="$(tput setaf 5)"     # shellcheck disable=SC2034
     cyan="$(tput setaf 6)"
     white="$(tput setaf 7)"
   # Set Color Theme - used by echo statements
     CLRnormal=${bold}${white}
     CLRheading=${bold}${green}
-    CLRheading2=${bold}${blue}
+    CLRheading2=${bold}${blue}    # shellcheck disable=SC2034
     CLRtitle=${bold}${cyan}
-    CLRtitle2=${bold}${yellow}
-    CLRsuccess=${bold}${green}
-    CLRwarning=${bold}${yellow}
-    CLRerror=${bold}${red}
+    CLRtitle2=${bold}${yellow}    # shellcheck disable=SC2034
+    CLRsuccess=${bold}${green}    # shellcheck disable=SC2034
+    CLRwarning=${bold}${yellow}   # shellcheck disable=SC2034
+    CLRerror=${bold}${red}        # shellcheck disable=SC2034
   fi
 fi
 
@@ -43,5 +43,6 @@ fi
 ###   COMMON FUNCTIONS
 
 display_ver() {
+  # shellcheck disable=SC2154
   echo -e "${CLRheading}${scriptname}${CLRnormal}  ver ${CLRtitle}${scriptbuildnum} ${CLRnormal}- ${CLRtitle}${scriptbuilddate}${CLRnormal}"
 }
